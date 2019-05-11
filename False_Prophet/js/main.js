6889dc934d8191159cdd21d221a7dd153bbefbdc
gaachave <gaachave@ucsc.edu>
2019-05-11 12:59:36 -0700

    Added comments for submiting
    Added our names, team name, and link to github.

--- False_Prophet/js/main.js
+++ False_Prophet/js/main.js
@@ -1,3 +1,7 @@
+//Team 25
+//Names: Garrett Chavez, David Hunt, and Jordan Lee
+//Link to github: https://github.com/ggchav/False_Prophet_Files.git
+
 var game = new Phaser.Game(1200, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 //initialize all states
